da03971696ae17ce81630071e2ce4ad1b363ff08
Stewart Lynch <slynch@createchsol.com>
2026-09-21 18:59:58 -0700

    Initial Commit

---
+++ MyToDos/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//----------------------------------------------
+// Original project: MyToDos
+// by  Stewart Lynch on 2026-09-21
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+//----------------------------------------------
+// Copyright © 2026 CreaTECH Solutions. All rights reserved.
+
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyToDos/MyToDosApp.swift
@@ -0,0 +1,25 @@
+//
+//----------------------------------------------
+// Original project: MyToDos
+// by  Stewart Lynch on 2026-09-21
+//
+// Follow me on Mastodon: @StewartLynch@iosdev.space
+// Follow me on Threads: @StewartLynch (https://www.threads.net)
+// Follow me on X: https://x.com/StewartLynch
+// Follow me on LinkedIn: https://linkedin.com/in/StewartLynch
+// Subscribe on YouTube: https://youTube.com/@StewartLynch
+// Buy me a ko-fi:  https://ko-fi.com/StewartLynch
+//----------------------------------------------
+// Copyright © 2026 CreaTECH Solutions. All rights reserved.
+
+
+import SwiftUI
+
+@main
+struct MyToDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
